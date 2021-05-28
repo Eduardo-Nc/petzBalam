796affa4048db7b5378768ebb7842dde9a0e8de6
Luis eduardo negron chan <eduardonegron649@gmail.com>
2021-05-27 20:59:32 -0500

    v17

--- src/componentes/masterPlan/masterPlan.js
+++ src/componentes/masterPlan/masterPlan.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './masterPlan.css';
 import master from '../../imagenes/07.svg';
 
-const MasterPlan = () =>{
-    return(
-        <img style={{width: '100vw'}} src={master}  alt="master Plan"  />
+const MasterPlan = () => {
+    return (
+        <section id="master-plan">
+            <img style={{ width: '100vw' }} src={master} alt="master Plan" />
+        </section>
+
     )
 }
 
